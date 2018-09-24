806ff5672cb35e1f48290472277f18bd073658c8
anjayluh <anjayluh.wakabi@gmail.com>
2018-09-24 11:28:02 +0300

    Refactor tests

--- test_api.py
+++ test_api.py
@@ -4,44 +4,44 @@ from flask import Flask, request
 import requests
 import json
 
-
 class TestOrdersUsingRequests(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
     def test_abort_if_order_doesnt_exist(self):
-        response = requests.get('http://127.0.0.1:5000/v1/orders/order10')
+        response = self.client.get('/v1/orders/order10')
         self.assertEqual(response.status_code, 404)
     def test_get_order_id(self):
-        response = requests.get('http://127.0.0.1:5000/v1/orders/order1')
+        response = self.client.get('/v1/orders/order1')
         self.assertEqual(response.status_code, 200)
         print(response.status_code)
     #test get all orders
     def test_get_orders(self):
-        response = requests.get('http://127.0.0.1:5000/v1/orders')
+        response = self.client.get('/v1/orders')
         self.assertEqual(response.status_code, 200)
         print(response.status_code)
 
     #test post id
     def test_put_order(self):
         order = {'order9':{'my_order': "Large size pizza"}}
-        response = requests.put('http://127.0.0.1:5000/v1/orders/<order>')
+        response = self.client.put('/v1/orders/<order>')
         self.assertEqual(response.status_code, 201)
         print(response.status_code)
 
     #test post order
     def test_post_order(self):
         orders = {'order3':{'my_order': "Noodles"}}
-        response = requests.post('http://127.0.0.1:5000/v1/orders')
+        response = self.client.post('http://127.0.0.1:5000/v1/orders')
         self.assertEqual(response.status_code, 201)
         print(response.status_code)
     
     #test delete
     def test_delete_order(self):
-        response = requests.delete('http://127.0.0.1:5000/v1/orders/order0')
+        response = self.client.delete('http://127.0.0.1:5000/v1/orders/order0')
         print(response)
         self.assertEqual(response.status_code, 204)
         print(response.status_code)
 
-    #test delete
-    '''def test_delete(self):'''
 if __name__ == "__main__":
     unittest.main()
     #Order = Order()
